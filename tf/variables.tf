6f280f0a99605137079dd8d426a8f633afcfcfdd
pawelad <pawel.ad@gmail.com>
2022-05-02 22:26:24 +0200

    Add Terraform files

---
+++ tf/variables.tf
@@ -0,0 +1,9 @@
+variable "cloudflare_email" {
+  type        = string
+  description = "Cloudflare account email."
+}
+
+variable "cloudflare_api_key" {
+  type        = string
+  description = "Cloudflare API key."
+}
