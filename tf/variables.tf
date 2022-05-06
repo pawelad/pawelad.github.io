94858e05d187ee8583e87931be62ec1ae5fae5c8
pawelad <pawel.ad@gmail.com>
2022-05-06 17:52:59 +0200

    Remove Terraform files

--- tf/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudflare_email" {
-  type        = string
-  description = "Cloudflare account email."
-}
-
-variable "cloudflare_api_key" {
-  type        = string
-  description = "Cloudflare API key."
-}
